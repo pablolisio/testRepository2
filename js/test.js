15dc5fa233b1e916830e60f69272428f1e56bbb4
pablolisio <pablolisio@gmail.com>
2014-02-27 19:12:31 -0300

    test

--- js/test.js
+++ js/test.js
@@ -7,3 +7,12 @@ $.post("http://compremosbien.com/bus", {
 				alert(data.val1);
            }, "json");
 }
+
+
+function getDataFromJson() { 
+// Haces la consulta al servidor
+$.getJSON('data/data.json', function(json) {
+                            alert(json.sites.site[0].expiryDate);
+                        });
+}
+
