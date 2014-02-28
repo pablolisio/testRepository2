757a166adbdf50d1df632cd230ccf735f4e238ce
pablolisio <pablolisio@gmail.com>
2014-02-28 18:33:16 -0300

    test

--- js/test.js
+++ js/test.js
@@ -10,30 +10,9 @@ $.post("http://compremosbien.com/bus", {
 }
 
 
-function getDataFromJson() { 
+function getDataFromJson() {
+alert("oijaijaoij");
 $.getJSON('data/data.json', function(json) {
                             alert(json.sites.site[0].expiryDate);
                         });
-}
-
-
-function wtf() {
-alert("entro");
-var textFile = null,
-  makeTextFile = function (text) {
-    var data = new Blob([text], {type: 'text/plain'});
-
-    // If we are replacing a previously generated file we need to
-    // manually revoke the object URL to avoid memory leaks.
-    if (textFile !== null) {
-      window.URL.revokeObjectURL(textFile);
-    }
-	alert(text);
-    textFile = window.URL.createObjectURL(data);
-
-    return textFile;
-  };
-  alert("japlaja");
- alert(makeTextFile("pablO"));
- alert("japlaja2");
 }
